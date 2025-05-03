36f112b7a90595581c60c2baccae9e56f0886791
garciamendes <garciaprog0101@gmail.com>
2025-05-03 20:52:23 -0300

    chore: creating the CD and versioning the system for deployment on GitHub Pages.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { AppVersion } from './components/version'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,6 +27,8 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Hello</p>
+
+      <AppVersion />
     </>
   )
 }
